176a043a751e380df8b1748c5814a8d22425b8c0
amberamigo <himanshu180599@gmail.com>
2021-01-20 20:15:58 +0530

    Initial commit

---
+++ profile-frontend/src/data/config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  // TODO: change your profile information here
+  name: "Himanshu Singh",
+  greeting: "Hey 👋",
+  greetingDescription: "I'm Himanshu Singh and I'm a Software Engineer!",
+  githubUrl: "https://github.com/amberamigo",
+  linkedinUrl: "https://www.linkedin.com/in/himanshu-singh-059a001a9/",
+  cvLink: "https://drive.google.com/file/d/1Zgf3R1SdtDE0P_53PbtcG0kgIe42yvrA/view?usp=sharing",
+};
